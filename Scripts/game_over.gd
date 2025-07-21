14d8b0d7543b0d337f516d4941f65eea52ecc8d3
tenchijin77 <rwilkinson@nctlv.org>
2025-07-21 15:28:52 -0700

    added main menu and fonts
    -added main menu with background music, added fonts to the wall of heroes, added background panel to wall of heroes and a quit button

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -70,3 +70,7 @@ func _on_submit_button_pressed():
 func _on_restart_button_pressed():
 	Global.reset()
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
---
+++ Scripts/main_menu.gd
@@ -0,0 +1,28 @@
+#main_menu.gd - handles main menu interactions
+extends Control
+
+@onready var menu_container : VBoxContainer = $menu_container
+@onready var start_button : Button = $menu_container/start_button
+@onready var quit_button : Button = $menu_container/quit_button
+@onready var background_music : AudioStreamPlayer = $background_music
+
+func _ready():
+	if not menu_container:
+		push_error("MainMenu: menu_container is null!")
+	if not start_button:
+		push_error("MainMenu: start_button is null!")
+	if not quit_button:
+		push_error("MainMenu: quit_button is null!")
+	if not background_music:
+		push_error("MainMenu: background_music is null!")
+	else:
+		background_music.play()
+		print("MainMenu: Playing background music")
+	if start_button:
+		start_button.grab_focus()
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
