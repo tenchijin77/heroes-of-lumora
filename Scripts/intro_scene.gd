e42e13179af2f397f6fb08bef0ca4df6e4be54f6
tenchijin77 <rwilkinson@nctlv.org>
2025-07-30 13:12:29 -0700

    updated intro dialogue
    -updated intro dialogue

--- Scripts/intro_scene.gd
+++ Scripts/intro_scene.gd
@@ -4,6 +4,11 @@ extends Node
 func _ready():
 	DialogicManager.connect("timeline_ended", Callable(self, "_on_dialogue_finished"))
 	DialogicManager.start_timeline("res://Dialogic/Timot-Intro.dtl")
+	var dialog_node = DialogicManager.get_dialog_node()
+	if dialog_node:
+		for s in dialog_node.get_signal_list():
+			print("Signal:", s.name)
+
 
 func _on_dialogue_finished():
 	print("Timeline ended!")
