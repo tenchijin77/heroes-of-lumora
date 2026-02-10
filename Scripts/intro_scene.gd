15e8cb2e26462409a1fb7459bb5e5041ec2eb3d2
tenchijin77 <rwilkinson@nctlv.org>
2026-02-09 18:51:53 -0800

    -updated the main introduction dialog
    -updated the main dialogue, updated project to godot 4.6

--- Scripts/intro_scene.gd
+++ Scripts/intro_scene.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _ready():
 	DialogicManager.connect("timeline_ended", Callable(self, "_on_dialogue_finished"))
-	DialogicManager.start_timeline("res://Dialogic/Timot-Intro.dtl")
+	DialogicManager.start_timeline("res://Dialogic/Timot-Intro2.dtl")
 	var dialog_node = DialogicManager.get_dialog_node()
 	if dialog_node:
 		for s in dialog_node.get_signal_list():
