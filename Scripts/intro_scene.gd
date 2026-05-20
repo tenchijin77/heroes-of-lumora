e9519f4a584d49a40fbc7f07a46b1443b6ec5f5d
tenchijin77 <rwilkinson@nctlv.org>
2026-05-20 16:01:11 -0700

    updated intro
    -updated intro text and music looping

--- Scripts/intro_scene.gd
+++ Scripts/intro_scene.gd
@@ -2,8 +2,14 @@
 extends Node
 
 func _ready():
+	var music := AudioStreamPlayer.new()
+	music.stream = load("res://Assets/Audio/HeroesOfLumora-STMix.ogg")
+	music.finished.connect(music.play)
+	add_child(music)
+	music.play()
+
 	DialogicManager.connect("timeline_ended", Callable(self, "_on_dialogue_finished"))
-	DialogicManager.start_timeline("res://Dialogic/Timot-Intro2.dtl")
+	DialogicManager.start_timeline("res://Dialogic/Timot-Intro.dtl")
 	var dialog_node = DialogicManager.get_dialog_node()
 	if dialog_node:
 		for s in dialog_node.get_signal_list():
